3baf0e7da5ca7bac54f8710b78f4a74dc1385fbc
alobcano <alejandro_obando@outlook.com>
2021-06-12 00:36:23 -0400

    Added auth token

--- src/Pages/Auth.js
+++ src/Pages/Auth.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 import "./Auth.css";
+import AuthContext from "../context/auth-context";
 
 class AuthPage extends React.Component {
   state = {
     isLogin: true,
   };
+
+  static contextType = AuthContext;
 
   constructor(props) {
     super(props);
@@ -67,7 +70,13 @@ class AuthPage extends React.Component {
         return res.json();
       })
       .then((resData) => {
-        console.log(resData);
+        if (resData.data.login.token) {
+          this.context.login(
+            resData.data.login.token,
+            resData.data.login.userId,
+            resData.data.login.tokenExpiration
+          );
+        }
       })
       .catch((err) => {
         console.log(err);
